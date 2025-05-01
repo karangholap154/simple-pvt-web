e1f40921c9fcfaeccb073a449574f737d8d59d2e
karangholap154 <karangholap154@gmail.com>
2025-05-01 22:47:56 +0530

    New pyq added

--- src/data/questionPapersData.ts
+++ src/data/questionPapersData.ts
@@ -3,56 +3,23 @@ import { QuestionPaper } from '../types';
 export const questionPapers: QuestionPaper[] = [
   {
     id: '1',
-    title: 'Data Structures',
+    title: 'High Performance Computing',
     branch: 'Computer',
-    semester: 3,
-    pdfFile: '/papers/data-structures-may-2023.pdf',
+    semester: 8,
+    pdfFile: './pyqs/comp/sem8/BE COMP HPC MAY 2023.pdf',
     date: {
-      month: 'May',
+      month: '-',
       year: 2023
     }
   },
   {
     id: '2',
-    title: 'Data Structures',
-    branch: 'Computer',
-    semester: 3,
-    pdfFile: '/papers/data-structures-dec-2022.pdf',
-    date: {
-      month: 'December',
-      year: 2022
-    }
-  },
-  {
-    id: '3',
-    title: 'Operating Systems',
-    branch: 'Computer',
-    semester: 4,
-    pdfFile: '/papers/os-may-2023.pdf',
-    date: {
-      month: 'May',
-      year: 2023
-    }
-  },
-  {
-    id: '4',
-    title: 'Operating Systems',
-    branch: 'Computer',
-    semester: 4,
-    pdfFile: '/papers/os-dec-2022.pdf',
-    date: {
-      month: 'December',
-      year: 2022
-    }
-  },
-  {
-    id: '5',
-    title: 'High Performance Computing',
+    title: 'Deep Learning',
     branch: 'Computer',
     semester: 8,
-    pdfFile: 'C:\Users\KARAN\Desktop\All PVT Website\pvtacademy-bolt\src\data\pyqs\comp\sem8\BE COMP HPC MAY 2023.pdf',
+    pdfFile: './pyqs/comp/sem8/BE COMP DL JUNE 2023.pdf',
     date: {
-      month: '-',
+      month: 'June',
       year: 2023
     }
   }
