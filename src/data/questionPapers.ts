675ee879f3858b27474765b83a92f8858a0ac459
karangholap154 <karangholap154@gmail.com>
2025-08-06 20:01:49 +0530

    `Added and updated question papers data.`

--- src/data/questionPapers.ts
+++ src/data/questionPapers.ts
@@ -1,8 +1,338 @@
 import { QuestionPaper } from "../types";
 
 export const questionPapers: QuestionPaper[] = [
+  // SEMESTER 3
   {
     id: "1",
+    title: "Engineering Mathematics-III (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 3,
+    pdfFile: "/pyqs/comp/sem3/SE COMP M3 JAN 23.pdf",
+    date: {
+      month: "January",
+      year: 2023,
+    },
+  },
+  {
+    id: "2",
+    title: "Data Structure (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 3,
+    pdfFile: "/pyqs/comp/sem3/SE COMP DS FEB 23.pdf",
+    date: {
+      month: "January",
+      year: 2023,
+    },
+  },
+  {
+    id: "3",
+    title: "Discrete Structures & Graph Theory (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 3,
+    pdfFile: "/pyqs/comp/sem3/SE COMP DSGT JAN 23.pdf",
+    date: {
+      month: "January",
+      year: 2023,
+    },
+  },
+  {
+    id: "4",
+    title: "Digital Logic & Computer Architecture (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 3,
+    pdfFile: "/pyqs/comp/sem3/SE COMP DLCA FEB 23.pdf",
+    date: {
+      month: "January",
+      year: 2023,
+    },
+  },
+  {
+    id: "5",
+    title: "Digital Logic & Computer Architecture (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 3,
+    pdfFile: "/pyqs/comp/sem3/SE COMP DLCA MAY 23.pdf",
+    date: {
+      month: "May",
+      year: 2023,
+    },
+  },
+  {
+    id: "6",
+    title: "Computer Graphics (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 3,
+    pdfFile: "/pyqs/comp/sem3/SE COMP CG FEB 23.pdf",
+    date: {
+      month: "January",
+      year: 2023,
+    },
+  },
+  {
+    id: "7",
+    title: "Computer Graphics (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 3,
+    pdfFile: "/pyqs/comp/sem3/SE COMP CG JUNE 23.pdf",
+    date: {
+      month: "May",
+      year: 2023,
+    },
+  },
+
+  // SEMESTER 4
+  {
+    id: "8",
+    title: "Engineering Mathematics-IV (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 4,
+    pdfFile: "/pyqs/comp/sem4/SE COMP M4 MAY 23.pdf",
+    date: {
+      month: "May",
+      year: 2023,
+    },
+  },
+  {
+    id: "9",
+    title: "Analysis of Algorithm (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 4,
+    pdfFile: "/pyqs/comp/sem4/SE COMP AOA MAY 23.pdf",
+    date: {
+      month: "May",
+      year: 2023,
+    },
+  },
+  {
+    id: "10",
+    title: "Database Management System (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 4,
+    pdfFile: "/pyqs/comp/sem4/SE COMP DBMS MAY 23.pdf",
+    date: {
+      month: "May",
+      year: 2023,
+    },
+  },
+  {
+    id: "11",
+    title: "Operting System (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 4,
+    pdfFile: "/pyqs/comp/sem4/SE COMP OS MAY 23.pdf",
+    date: {
+      month: "May",
+      year: 2023,
+    },
+  },
+  {
+    id: "12",
+    title: "Microprocessors (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 4,
+    pdfFile: "/pyqs/comp/sem4/SE COMP MP MAY 23.pdf",
+    date: {
+      month: "May",
+      year: 2023,
+    },
+  },
+
+  // SEMESTER 5
+  {
+    id: "13",
+    title: "Theoretical Computer Science (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 5,
+    pdfFile: "/pyqs/comp/sem5/TE COMP TCS NOV 23.pdf",
+    date: {
+      month: "November",
+      year: 2023,
+    },
+  },
+  {
+    id: "14",
+    title: "Theoretical Computer Science (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 5,
+    pdfFile: "/pyqs/comp/sem5/TE COMP TCS JUNE 24.pdf",
+    date: {
+      month: "June",
+      year: 2024,
+    },
+  },
+  {
+    id: "15",
+    title: "Software Engineering (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 5,
+    pdfFile: "/pyqs/comp/sem5/TE COMP SE NOV 23.pdf",
+    date: {
+      month: "November",
+      year: 2023,
+    },
+  },
+  {
+    id: "16",
+    title: "Software Engineering (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 5,
+    pdfFile: "/pyqs/comp/sem5/TE COMP SE JUNE 24.pdf",
+    date: {
+      month: "June",
+      year: 2024,
+    },
+  },
+  {
+    id: "17",
+    title: "Data Warehousing & Mining (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 5,
+    pdfFile: "/pyqs/comp/sem5/TE COMP DWM JUNE 24.pdf",
+    date: {
+      month: "June",
+      year: 2024,
+    },
+  },
+  {
+    id: "18",
+    title: "Internet Programming (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 5,
+    pdfFile: "/pyqs/comp/sem5/TE COMP IP JUNE 24.pdf",
+    date: {
+      month: "June",
+      year: 2024,
+    },
+  },
+
+  // SEMESTER 6
+  {
+    id: "19",
+    title: "System Programming & Compiler Construction (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 6,
+    pdfFile: "/pyqs/comp/sem6/TE COMP SPCC MAY 24.pdf",
+    date: {
+      month: "May",
+      year: 2024,
+    },
+  },
+  {
+    id: "20",
+    title: "System Programming & Compiler Construction (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 6,
+    pdfFile: "/pyqs/comp/sem6/TE COMP SPCC NOV 24.pdf",
+    date: {
+      month: "November",
+      year: 2024,
+    },
+  },
+  {
+    id: "21",
+    title: "Cryptography & System Security (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 6,
+    pdfFile: "/pyqs/comp/sem6/TE COMP CSS MAY 24.pdf",
+    date: {
+      month: "May",
+      year: 2024,
+    },
+  },
+  {
+    id: "22",
+    title: "Mobile Computing (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 6,
+    pdfFile: "/pyqs/comp/sem6/TE COMP MC MAY 24.pdf",
+    date: {
+      month: "May",
+      year: 2024,
+    },
+  },
+  {
+    id: "23",
+    title: "Artificial Intelligence (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 6,
+    pdfFile: "/pyqs/comp/sem6/TE COMP AI MAY 24.pdf",
+    date: {
+      month: "May",
+      year: 2024,
+    },
+  },
+  {
+    id: "24",
+    title: "Internet of Things (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 6,
+    pdfFile: "/pyqs/comp/sem6/TE COMP IOT MAY 24.pdf",
+    date: {
+      month: "May",
+      year: 2024,
+    },
+  },
+
+  // SEMESTER 7
+  {
+    id: "25",
+    title: "Big Data Analytics (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 7,
+    pdfFile: "/pyqs/comp/sem7/BE COMP BDA NOV 24.pdf",
+    date: {
+      month: "November",
+      year: 2024,
+    },
+  },
+  {
+    id: "26",
+    title: "Machine Learning (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 7,
+    pdfFile: "/pyqs/comp/sem7/BE COMP ML NOV 24.pdf",
+    date: {
+      month: "November",
+      year: 2024,
+    },
+  },
+  {
+    id: "27",
+    title: "Information Retrieval (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 7,
+    pdfFile: "/pyqs/comp/sem7/BE COMP IR NOV 24.pdf",
+    date: {
+      month: "November",
+      year: 2024,
+    },
+  },
+  {
+    id: "28",
+    title: "Natural Language Processing (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 7,
+    pdfFile: "/pyqs/comp/sem7/BE COMP NLP NOV 24.pdf",
+    date: {
+      month: "November",
+      year: 2024,
+    },
+  },
+  {
+    id: "29",
+    title: "Cyber Security Laws (REV-2019 ‘C’ SCHEME)",
+    branch: "Computer",
+    semester: 7,
+    pdfFile: "/pyqs/comp/sem7/BE COMP CSL NOV 24.pdf",
+    date: {
+      month: "November",
+      year: 2024,
+    },
+  },
+
+  // SEMESTER 8
+  {
+    id: "30",
     title: "Deep Learning (REV-2019 ‘C’ SCHEME)",
     branch: "Computer",
     semester: 8,
@@ -13,7 +343,7 @@ export const questionPapers: QuestionPaper[] = [
     },
   },
   {
-    id: "2",
+    id: "31",
     title: "High Performance Computing (REV-2019 ‘C’ SCHEME)",
     branch: "Computer",
     semester: 8,
@@ -24,22 +354,22 @@ export const questionPapers: QuestionPaper[] = [
     },
   },
   {
-    id: "3",
+    id: "32",
     title: "Fianance Management (REV-2019 ‘C’ SCHEME)",
     branch: "Computer",
     semester: 8,
-    pdfFile: "/pyqs/comp/sem8/",
+    pdfFile: "/pyqs/comp/sem8/BE COMP FM June 2023.pdf",
     date: {
       month: "June",
       year: 2023,
     },
   },
   {
-    id: "4",
+    id: "33",
     title: "Fianance Management (REV-2019 ‘C’ SCHEME)",
     branch: "Computer",
     semester: 8,
-    pdfFile: "/pyqs/comp/sem8/BE COMP HPC MAY 2023.pdf",
+    pdfFile: "/pyqs/comp/sem8/BE COMP FM JUNE 2024.pdf",
     date: {
       month: "June",
       year: 2024,
